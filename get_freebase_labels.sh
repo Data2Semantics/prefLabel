838393c8e6945ab239d3beea37dafc390547d442
Marat Charlaganov <charlaganov@gmail.com>
2014-01-14 18:50:30 +0100

    Make sure we don't get en-GB labels

--- get_freebase_labels.sh
+++ get_freebase_labels.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl http://commondatastorage.googleapis.com/freebase-public/rdf/freebase-rdf-2013-12-22-00-00.gz | gzip -dcf | grep "http://www.w3.org/2000/01/rdf-schema#label" | grep "\"@en\b"
+curl http://commondatastorage.googleapis.com/freebase-public/rdf/freebase-rdf-2013-12-22-00-00.gz | gzip -dcf | grep "http://www.w3.org/2000/01/rdf-schema#label" | grep "\"@en\s*[.]\s*$"
