a8d3b79d3aef4a5a5805e016e88d77c1934f5cac
Marat Charlaganov <charlaganov@gmail.com>
2014-05-15 15:55:37 +0200

    Basic authentication

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python2.7
 import couchdbkit
+import restkit
+import sys
 from couchdbkit.designer import push
+if len(sys.argv) == 3:
+    username, password = sys.argv[1:3]
+    filters = [restkit.BasicAuth(username, password)]
+else:
+    filters = None
 dbname = 'preflabel'
-server = couchdbkit.Server()
+server = couchdbkit.Server(filters=filters)
 db = server.get_or_create_db(dbname)
 push('./_design/index', db)
