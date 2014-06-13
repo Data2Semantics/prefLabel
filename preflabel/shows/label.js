d080c272ef955f99a572ecf997c304abaa6b5bff
Marat Charlaganov <charlaganov@gmail.com>
2014-06-13 19:14:59 +0200

    Include provenance link in /api/label response header

--- preflabel/shows/label.js
+++ preflabel/shows/label.js
@@ -1,17 +1,27 @@
 function(doc, req){
   var headers = {};
   if (doc) {
-    if (req.headers["Accept"].match(/json/)) {
+    var prov = ''
+    if (doc['prov']) {
+      prov = doc['prov'] + '; rel="http://www.w3.org/ns/prov#has_provenance" '
+    }
+    if (req.headers['Accept'].match(/json/)) {
       return {body: JSON.stringify({'label': doc['en']}),
-              headers: {"Content-Type": "application/json"}};
+              headers: {
+                'Content-Type': 'application/json',
+                'Link' : prov
+              }};
     } else {
       return {body: doc['en'],
-              headers: {"Content-Type": "text/plain"}};
+              headers: {
+                'Content-Type': 'text/plain',
+                'Link' : prov
+              }};
     }
   } else {
     return {
-      body: JSON.stringify( {"error": "not_found", "reason": "no data for <" + req.id + ">"}),
-      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify( {'error': 'not_found', 'reason': 'no data for <' + req.id + '>'}),
+      headers: {'Content-Type': 'application/json'},
       code : 404
     };
   };
