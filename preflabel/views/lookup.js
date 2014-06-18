415e2f8f71cfc57433922542173bf893fc81f1f8
Marat Charlaganov <charlaganov@gmail.com>
2014-06-18 11:51:42 +0200

    Delete unused lookup.js

--- preflabel/views/lookup.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-  languages = ['en', 'fr'];
-  for (var i = 0; i < languages.length(); i++) {
-    lang = languages[i];
-    if (doc[lang]) {
-      emit([lang, doc[lang]]);
-    }
-  }
-}
