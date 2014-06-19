b0a7b6ccf16aa51fef3fa80043eb8618f8ab90ce
Marat Charlaganov <charlaganov@gmail.com>
2014-06-20 00:12:52 +0200

    Load all languages

--- scripts/load_freebase.sh
+++ scripts/load_freebase.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOG="freebase.log"
 
 echo "`date` Loading Freebase."
-curl -s http://commondatastorage.googleapis.com/freebase-public/rdf/freebase-rdf-latest.gz 2>> "$LOG" | gzip -dcf  2>> "$LOG" | grep "http://www.w3.org/2000/01/rdf-schema#label" | grep "\"@en\s*[.]\s*$" | python2.7 "$SCRIPT_DIR/loader.py" &>> "$LOG"
+curl -s http://commondatastorage.googleapis.com/freebase-public/rdf/freebase-rdf-latest.gz | gzip -dcf | grep "http://www.w3.org/2000/01/rdf-schema#label" | sort -u -S 1G - |python2.7 "$SCRIPT_DIR/loader.py" &>> "$LOG"
 echo "`date` Complete."
