76c1be274bd8bd96c4ae524555f43b8ac030368f
Marat Charlaganov <charlaganov@gmail.com>
2014-06-27 17:36:51 +0200

    Minor fixes

--- preflabel/views/lookup/map.js
+++ preflabel/views/lookup/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   languages = ['en', 'fr'];
-  for (var i = 0; i < languages.length(); i++) {
+  for (var i = 0; i < languages.length; i++) {
     lang = languages[i];
     if (doc[lang]) {
       emit([lang, doc[lang]]);
